4348fee27fab79589343ae36c9a01fc78b45d64b
Jin Oh <pinion0801@gmail.com>
2022-06-24 14:48:46 +0900

    :bug: public route 버그 수정

--- src/App.js
+++ src/App.js
@@ -11,22 +11,16 @@ function App() {
   return (
       <Router>
         <Routes>
-            {/*<Route path="/" element={<Home />} />*/}
-            {/*<Route path="/login" element={<Login />} />*/}
-            {/*<Route path="/logout" element={<Logout />} />*/}
             <Route element={<PrivateRoute />}>
                 <Route path="/" element={<Home />} />
                 <Route path="/logout" element={<Logout />} />
             </Route>
 
-            <Route element={<PublicRoute />}>
-                <Route path="/user/login" element={<Login />} />
-            </Route>
-            {/*<Route path="/user/login"*/}
-            {/*       element={<PublicRoute>*/}
-            {/*                   <Login />*/}
-            {/*               </PublicRoute>}*/}
-            {/*/>*/}
+            <Route path="/user/login"
+                   element={
+                        <PublicRoute>
+                            <Login />
+                        </PublicRoute>} />
         </Routes>
 
       </Router>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 
